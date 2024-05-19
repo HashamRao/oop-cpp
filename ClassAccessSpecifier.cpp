6bfd71301f84af7be23c073679ff10ed986e2a11
HashamRao <hasham.rao01@gmail.com>
2024-05-19 12:19:20 +0500

    add notes for class access specifiers

---
+++ ClassAccessSpecifier.cpp
@@ -0,0 +1,7 @@
+// Access specifiers set the accessibility of classes , methods and other members.
+/* class access specifiers determines how private protected and public members of 
+base class are inherited by the derived class.
+class access specifiers are of 3 types 
+->public    :: public members of the base class will be inhereited as public members to derived class
+and protected members as protected members. */ 
+
